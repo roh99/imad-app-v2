42aedfc68e7c8787b2bdb6413c51c33bb9e7e520
roh99 <garg.rohan819@gmail.com>
2017-03-11 17:56:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send("Article one will be served here");
+ res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 
 app.get('/article-two', function (req, res) {
-  res.send("Article two will be served here");
+ res.send("Article two will be served here"); 
 });
 
 app.get('/article-three', function (req, res) {
-  res.send("Article one will be served here");
+  res.send("Article three will be served here");
 });
 
 app.get('/ui/style.css', function (req, res) {
